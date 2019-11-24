77775fc829120ea2c95a6b5ce0bb1f4feacd06a4
blackrose9 <sarah.mwendwa@gmail.com>
2019-11-25 00:14:05 +0300

    Change index.html picture and sort a few bugs in the display

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ function scrollFunction() {
 // if page is scrolled down 70px from the top of the page then... 
   if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
    // getElementbyId="navbar" and change padding of navbar and height and width of img Id="me" 
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("me").style.height = "50px";
+    document.getElementById("navbar").style.padding = "0px 10px";
+    document.getElementById("me").style.height = "100px";
     document.getElementById("me").style.width = "100px";
    // else change navbar padding, and height and width of img Id="me"
   } else {
-    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("navbar").style.padding = "20px 10px";
     document.getElementById("me").style.height = "150px";
-    document.getElementById("me").style.width = "250px";
+    document.getElementById("me").style.width = "150px";
   }
 }
